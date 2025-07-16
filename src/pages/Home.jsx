0450370cb06577e377cac1500c9e2b469e60dfa3
Rakibul Islam Sumon <rakibuldevs@gmail.com>
2025-07-16 01:31:29 -0700

    add carosel section to the website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import VideoPlayer from "../components/sections/VideoPlayer";
 import PricingSection from "../components/sections/PricingSection";
 import TestimonialSection from "../components/sections/TestimonialSection";
 import Testimonial from "../components/sections/Testimonial";
+
+import Carousel from "../components/sections/Carousel";
+
 const Home = () => {
   return (
     <>
@@ -13,6 +16,7 @@ const Home = () => {
       <AudienceSection />
       <VideoPlayer isGradient={true} />
       <PricingSection />
+ <Carousel/>
       <Testimonial/>
       <TestimonialSection />
     </>
